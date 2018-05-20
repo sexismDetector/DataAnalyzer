73d2576d230dcff776a85a4513e83dee761eada6
acevedo-oscar <oscar.acevedo@comunidad.unam.mx>
2018-05-20 14:42:20 +0900

    First big change

--- src/predicter.py
+++ src/predicter.py
@@ -1,10 +1,7 @@
 """
-Script that makes predcitions based on comments
+Script that makes predictions based on comments
 
 """
-
-
-
 from keras.preprocessing.text import Tokenizer
 from keras import preprocessing
 from keras.preprocessing.sequence import pad_sequences
@@ -12,34 +9,34 @@ from keras.models import load_model
 
 from sys import argv
 
-
 def Tokenize_New_Instance(new_string, text_length):
 
-	new_instance = []
+    new_instance = []
 
-	new_instance.append(new_string)
+    new_instance.append(new_string)
 
-	# create the tokenizer
-	new_tok= Tokenizer()
-	# fit the tokenizer on the documents
-	new_tok.fit_on_texts(new_instance)
+    # create the tokenizer
+    new_tok= Tokenizer()
+    # fit the tokenizer on the documents
+    new_tok.fit_on_texts(new_instance)
 
-	tokenized_text = pad_sequences( new_tok.texts_to_sequences(new_instance) , maxlen=text_length)
+    tokenized_text = pad_sequences( new_tok.texts_to_sequences(new_instance) , maxlen=text_length)
 
-	return tokenized_text
- 
+    return tokenized_text
 
-model =  load_model('/home/acevedo/Yonsei/Data_Mining/DataAnalyzer/src/sexism_classifier.h5')
+model = load_model('sexism_classifier.h5')
 
-print("Loading Model Completed")
+while True:
 
-raw_text = str( argv[1])
+	raw_text =
+	input_prediction = model.predict( Tokenize_New_Instance(raw_text, 200) )
 
+
+
+"""
+raw_text = argv[1]
 print("Given text is "+str( argv[1]))
-
-input_prediction = model.predict( Tokenize_New_Instance(raw_text, 200) )
-
 print("\n")
-
 print("Final Result")
 print(input_prediction[0][0])
+"""
